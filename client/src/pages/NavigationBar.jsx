8f24baef17eed6a269a5d4b25571056a0b454c80
ninggg04 <ningming559@gmail.com>
2024-10-02 16:10:28 +0700

    new

--- client/src/pages/NavigationBar.jsx
+++ client/src/pages/NavigationBar.jsx
@@ -6,7 +6,6 @@ const NavigationBar = () => {
     <nav className="bg-black text-white flex justify-around p-4">
       <Link to="/" className="text-red-500 font-bold">หน้าหลัก</Link>
       <Link to="/concerts" className="hover:text-red-500">คอนเสิร์ต</Link>
-      <Link to="/notebooklub" className="hover:text-red-500">โน้ตคลับ</Link>
       <Link to="/artists" className="hover:text-red-500">ศิลปิน</Link>
       <Link to="/promotions" className="hover:text-red-500">โปรโมชั่น</Link>
       <Link to="/products" className="hover:text-red-500">สินค้า</Link>
