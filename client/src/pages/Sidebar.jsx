2e3348de9e6cfe5635af5dde71dae8ca888c0b2c
Thanakit Prasobthai <157215133+Thanakit7949@users.noreply.github.com>
2024-10-10 20:25:04 +0700

    eiei

--- client/src/pages/Sidebar.jsx
+++ client/src/pages/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <ul>
-        <li><Link to="/profile">โปรไฟล์</Link></li>
+        <li><Link to="/user/profile">โปรไฟล์</Link></li>
         <li><Link to="/concerts">คอนเสิร์ต</Link></li>
         <li><Link to="/sports">กีฬา</Link></li>
         {/* เพิ่มเมนูเพิ่มเติมตามที่คุณต้องการ */}
